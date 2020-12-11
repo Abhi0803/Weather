9956deb98776e379e21d7b61a372f4ff2fd885bd
Abhinav Jha <Abhi0803@github.com>
2020-12-12 00:37:26 +0530

    Updates

---
+++ precache-manifest.c6fda4af306bfc94b7d37d690224e756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f5ef76155f9443d4abcda66dc73582",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "2254d0bf2aec55749834",
+    "url": "/Weather/static/js/2.76baab58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.76baab58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3968c68fbd5f551096b7",
+    "url": "/Weather/static/js/main.4c2cad62.chunk.js"
+  },
+  {
+    "revision": "85c9d8bb56919458d0e6",
+    "url": "/Weather/static/js/runtime-main.1c36a231.js"
+  }
+]);
